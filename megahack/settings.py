7144fe8bdda76cb6b3718c1f088a6ad4a01ba4ff
Chirag0401 <chip2741@gmail.com>
2021-03-11 00:32:15 +0530

    Whitenoise removed

--- megahack/settings.py
+++ megahack/settings.py
@@ -126,8 +126,5 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
 ]
 
-STATIC_ROOT= os.path.join(BASE_DIR,"static")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
